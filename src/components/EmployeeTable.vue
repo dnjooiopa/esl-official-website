a8ca8ee07b66228ab2212a0f71f5ea71194a6aa1
dnjooiopa <dnjooiopa@gmail.com>
2020-09-06 16:42:25 +0700

    style table

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -24,4 +24,21 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
