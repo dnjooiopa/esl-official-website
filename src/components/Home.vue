6a63c344aea952ca8fc3fcbd54ceb2d51fe80653
dnjooiopa <dnjooiopa@gmail.com>
2020-09-08 16:07:50 +0700

    adding image to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to Embedded System Laboratory</h1>
+    <img src="../assets/home-img.jpg" class="responsive" />
   </div>
 </template>
 
@@ -10,4 +10,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.responsive {
+  max-width: 100%;
+  height: auto;
+}
+</style>
