f8aa7ee3feba05644f9cc462a1ecff4089f146d7
dnjooiopa <dnjooiopa@gmail.com>
2020-09-10 10:25:04 +0700

    embeding carousel

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="activity-container">
-    <div class="activity-title" v-on:click="onClick">Activity & Awards</div>
-    
+    <div class="activity-title">Activity & Awards</div>
+    <br />
+    <div class="slider-container">
+      <b-carousel :pause-hover="pauseHover">
+        <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+          <section :class="`hero is-medium is-${carousel.color}`">
+            <div class="hero-body has-text-centered">
+              <h1 class="title">{{carousel.text}}</h1>
+            </div>
+          </section>
+        </b-carousel-item>
+      </b-carousel>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+
+Vue.use(Buefy);
+
 export default {
-  name: 'Activity',
-}
+  name: "Activity",
+  data() {
+    return {
+      pauseHover: false,
+      carousels: [
+        { text: "Slide 1", color: "primary" },
+        { text: "Slide 2", color: "info" },
+        { text: "Slide 3", color: "success" },
+        { text: "Slide 4", color: "warning" },
+        { text: "Slide 5", color: "danger" },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
 .activity-container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   margin-top: 108px;
 }
@@ -21,7 +51,11 @@ export default {
 .activity-title {
   font-size: 2.4rem;
   font-weight: 700;
-  font-family: 'Orbitron', sans-serif;
+  font-family: "Orbitron", sans-serif;
+}
+
+.slider-container {
+  width: 300px;
 }
 </style>
 
