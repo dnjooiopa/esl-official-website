db328e6e1491dc9aa6c0cc10ec6e130d17464bdb
dnjooiopa <dnjooiopa@gmail.com>
2020-09-15 21:57:08 +0700

    make brand text responsive for all screen sizes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex h-sm-custom lg:h-64">
     <img class="w-full object-cover object-center" src="../assets/images/home-img.jpg" />
-    <div class="absolute brand text-white">Embedded System Laboratory</div>
+    <div class="absolute w-full mt-32 text-white text-center md:mt-48 lg:mt-64">
+        <span class="text-4xl lg:text-5xl">Embedded <br class="md:hidden">System <br class="md:hidden">Laboratory</span>
+    </div>
   </div>
 </template>
 
@@ -16,11 +18,6 @@ export default {
   height: 30rem;
 }
 
-.brand {
-  left: 50%;
-  transform: translate(-50%, -50%);
-  top: 330px;
-}
 
 @media (min-width: 1024px) {
   .h-sm-custom {
