8a67c7a708c98458d5518200102b36ce27fc746f
dnjooiopa <dnjooiopa@gmail.com>
2020-09-16 16:09:23 +0700

    embed slider

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <Home />
     <Activity />
+    <Slider />
   </div>
 </template>
 
 <script>
 import Home from "./components/Home";
 import Activity from "./components/Activity";
+import Slider from "./components/Slider"
 
 export default {
   name: "App",
-  components: { Home, Activity },
+  components: { Home, Activity, Slider },
 };
 </script>
 
