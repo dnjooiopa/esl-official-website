2da9728406b4c168e8d4d915ca24778bc9d5323c
dnjooiopa <dnjooiopa@gmail.com>
2020-09-16 19:08:57 +0700

    adding slider state

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,40 +1,22 @@
 <template>
   <div class="flex flex-col items-center my-8">
     <div class="img-container bg-white rounded overflow-x-hidden flex snap-x">
-      <div
-        class="snap-start w-full h-full flex flex-shrink-0 relative"
-        id="slide-1"
-      >
+      <div class="snap-start w-full h-full flex flex-shrink-0 relative" id="slide-1">
         <img
           src="../assets/images/topgun-2020.jpg"
           class="h-full w-full object-cover absolute inset-0 z-10"
         />
       </div>
-      <div
-        class="snap-start w-full h-full flex flex-shrink-0 relative"
-        id="slide-2"
-      >
+      <div class="snap-start w-full h-full flex flex-shrink-0 relative" id="slide-2">
         <img
           src="../assets/images/silicon-craft.jpg"
           class="h-full w-full object-cover object-bottom absolute inset-0 z-10"
         />
       </div>
-      <div
-        class="snap-start w-full h-full flex flex-shrink-0 bg-red-600"
-        id="slide-3"
-      >Slide 3</div>
-      <div
-        class="snap-start w-full h-full flex flex-shrink-0 bg-orange-600"
-        id="slide-4"
-      >Slide 4</div>
-      <div
-        class="snap-start w-full h-full flex flex-shrink-0 bg-purple-600"
-        id="slide-5"
-      >Slide 5</div>
-      <div
-        class="snap-start w-full h-full flex flex-shrink-0 bg-black relative"
-        id="slide-6"
-      >
+      <div class="snap-start w-full h-full flex flex-shrink-0 bg-red-600" id="slide-3">Slide 3</div>
+      <div class="snap-start w-full h-full flex flex-shrink-0 bg-orange-600" id="slide-4">Slide 4</div>
+      <div class="snap-start w-full h-full flex flex-shrink-0 bg-purple-600" id="slide-5">Slide 5</div>
+      <div class="snap-start w-full h-full flex flex-shrink-0 bg-black relative" id="slide-6">
         <img
           src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
           class="h-full w-full object-cover absolute inset-0 z-10 opacity-25"
@@ -68,6 +50,14 @@
         class="w-8 h-8 text-gray-700 rounded-full bg-white flex justify-center items-center"
         href="#slide-6"
       >6</a>
+      <a
+        class="w-8 h-8 text-gray-700 rounded-full bg-white flex justify-center items-center"
+        :href="prev"
+      >prev</a>
+      <a
+        class="w-8 h-8 text-gray-700 rounded-full bg-white flex justify-center items-center"
+        :href="next"
+      >next</a>
     </div>
   </div>
 </template>
@@ -75,6 +65,12 @@
 <script>
 export default {
   name: "Slider",
+  data() {
+    return {
+      prev: "#slide-1",
+      next: "#slide-2",
+    };
+  },
 };
 </script>
 
